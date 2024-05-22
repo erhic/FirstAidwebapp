959a57cc4126b19cb39330a43e414e7e72bf8a4c
erhic <ericngugi24@gmail.com>
2024-05-22 10:51:06 +0300

    feat : add nav links constants

---
+++ src/Components/Navmenu.jsx
@@ -0,0 +1,31 @@
+export const NavMenu = [
+  { menuName: "Home", routePath: "/home" },
+  { menuName: "About", routePath: "/about" },
+  { menuName: "First Aid", routePath: "/firstaid" },
+  { menuName: "Emergency", routePath: "/emergency" },
+  { menuName: "Resouces", routePath: "/resources" },
+  { menuName: "Courses", routePath: "/courses" },
+];
+
+export const HomeMainContent = [
+  {
+    headerName: "First Aid Basics",
+    paragraphText: "Learn Fundamental principle of first aid.",
+    redirectLink: "",
+  },
+  {
+    headerName: "Emergency Situation",
+    paragraphText: "Step to step guides on handling various emergencies.",
+    redirectLink: "",
+  },
+  {
+    headerName: "Resources",
+    paragraphText: "Downloadable guides, videos, and more.",
+    redirectLink: "",
+  },
+  {
+    headerName: "Courses and Training",
+    paragraphText: "Find and enroll in first aid training courses.",
+    redirectLink: "",
+  },
+];
