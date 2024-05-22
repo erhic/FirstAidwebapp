aa73acc94ca3afe7ef6001f1e5a4f2b1237439f7
erhic <ericngugi24@gmail.com>
2024-05-22 11:29:25 +0300

    fix: edit constant file

---
+++ src/Constants/index.js
@@ -0,0 +1,60 @@
+export const NavMenu = [
+  { menuName: "Home", routePath: "/home" },
+  { menuName: "About", routePath: "/about" },
+  { menuName: "First Aid", routePath: "/firstaid" },
+  { menuName: "Emergency", routePath: "/emergency" },
+  { menuName: "Resouces", routePath: "/resources" },
+  { menuName: "Courses", routePath: "/courses" },
+];
+
+export const HomeMainContent = [
+  {
+    headingName: "First Aid Basics",
+    paragraphText: "Learn Fundamental principle of first aid.",
+    redirectLink: "",
+  },
+  {
+    headingName: "Emergency Situation",
+    paragraphText: "Step to step guides on handling various emergencies.",
+    redirectLink: "",
+  },
+  {
+    headingName: "Resources",
+    paragraphText: "Downloadable guides, videos, and more.",
+    redirectLink: "",
+  },
+  {
+    headingName: "Courses and Training",
+    paragraphText: "Find and enroll in first aid training courses.",
+    redirectLink: "",
+  },
+];
+
+export const Testimonial = [
+  {
+    clientName: "Jane",
+    clientStatement:
+      "Thanks to this website, I was able to help someone in a crisis!",
+    clientPhoto: "",
+  },
+  {
+    clientName: "John",
+    clientStatement: "The guides are easy to follow and very informative.",
+    clientPhoto: "",
+  },
+  {
+    clientName: "Rose",
+    clientStatement: "The guides are easy to follow and very informative.",
+    clientPhoto: "",
+  },
+];
+
+export const FooterLink = [
+  { linkName: "Email", linkIcon: "", linkContext: "" },
+  { linkName: "Phone", linkIcon: "", linkContext: "" },
+  { linkName: "Facebook", linkIcon: "", linkContext: "" },
+  { linkName: "Twitter", linkIcon: "", linkContext: "" },
+  { linkName: "Instagram", linkIcon: "", linkContext: "" },
+  { linkName: "Privacy Policy", linkIcon: "", linkContext: "" },
+  { linkName: "Term of Services", linkIcon: "", linkContext: "" },
+];
