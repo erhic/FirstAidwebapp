eecb5ecd8cd6467f28e94045f0f306cb458a9d06
erhic <ericngugi24@gmail.com>
2024-05-22 14:05:16 +0300

    feat: add router paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Firstaid from "./Pages/Firstaid.jsx";
+import Courses from "./Pages/Courses.jsx";
+import Resources from "./Pages/Resources.jsx";
+import Contactus from "./Pages/Contactus.jsx";
+import Emergency from "./Pages/Emergency.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/firstaid", element: <Firstaid /> },
+      { path: "/emergency", element: <Emergency /> },
+      { path: "/courses", element: <Courses /> },
+      { path: "/resources", element: <Resources /> },
+      { path: "/contactus", element: <Contactus /> },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
