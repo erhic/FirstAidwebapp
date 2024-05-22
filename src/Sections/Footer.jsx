cdacd251ec7c75d34a7ca4072159855f0eb5699b
erhic <ericngugi24@gmail.com>
2024-05-22 17:08:10 +0300

    feat:  style footer and navbar for pc devices

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -3,34 +3,43 @@ import { FooterLink } from "../Constants";
 const Footer = () => {
   return (
     <>
-      <section>
-        <h3>Contact Info</h3>
-        <ul>
-          {FooterLink.filter((item, index) => index <= 1).map((item) => (
-            <li key={item.linkContext}>
-              <a href={item.linkContext}>{item.linkName}</a>
-            </li>
-          ))}
-        </ul>
-
-        <h3>Social Media</h3>
-        <ul>
-          {FooterLink.filter((item, index) => index < 5 && index > 1).map(
-            (item) => (
+      <section className="w-full flex justify-around bg-slate-100 p-14 fontSize-base max-lg:flex-col ">
+        <div>
+          <h3 className="text-lg pb-4 text-black font-bold">Contact Info</h3>
+          <ul>
+            {FooterLink.filter((item, index) => index <= 1).map((item) => (
               <li key={item.linkContext}>
                 <a href={item.linkContext}>{item.linkName}</a>
               </li>
-            )
-          )}
-        </ul>
-        <h3>Additional Links</h3>
-        <ul>
-          {FooterLink.filter((item, index) => index > 4).map((item) => (
-            <li key={item.linkContext}>
-              <a href={item.linkContext}>{item.linkName}</a>
-            </li>
-          ))}
-        </ul>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-lg pb-4 text-black font-bold">Social Media</h3>
+          <ul>
+            {FooterLink.filter((item, index) => index < 5 && index > 1).map(
+              (item) => (
+                <li key={item.linkContext}>
+                  <a href={item.linkContext}>{item.linkName}</a>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-lg pb-4 text-black font-bold">
+            Additional Links
+          </h3>
+          <ul>
+            {FooterLink.filter((item, index) => index > 4).map((item) => (
+              <li key={item.linkContext}>
+                <a href={item.linkContext}>{item.linkName}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
     </>
   );
--- src/Sections/Navigation.jsx
+++ src/Sections/Navigation.jsx
@@ -3,12 +3,12 @@ import { firstaidkitelog } from "../assets/images";
 
 const Navigation = () => {
   return (
-    <nav>
-      <div className="logo">
+    <nav className="w-full flex  justify-between px-16 items-center bg-slate-100 p-6 ">
+      <div className="logo flex">
         <img src={firstaidkitelog} alt="" />
       </div>
       <div className="navlink">
-        <ul>
+        <ul className="flex gap-8 ">
           {navigationMenu.map((item) => (
             <li key={item.menuName}>
               <a href={item.routePath}>{item.menuName}</a>
