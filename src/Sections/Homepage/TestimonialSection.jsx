2bc77571db1610aea83653481329ab699b3cf02b
erhic <ericngugi24@gmail.com>
2024-05-23 07:53:14 +0300

    fix : updated typography on homepage

--- src/Sections/Homepage/TestimonialSection.jsx
+++ src/Sections/Homepage/TestimonialSection.jsx
@@ -6,26 +6,26 @@ const TestimonialSection = () => {
       <h3 className="pl-14 text-lg font-bold text-slate-600 pt-6">
         Testmonial
       </h3>
-      <section className="w-full flex gap-6 justify-center py-14 font-palanquin px-10">
+      <section className="w-full flex gap-6 justify-center pt-14 pb-16 font-palanquin px-10">
         <h3></h3>
         {HomeTestimonial.map((item) => (
           <div
             key={item.clientStatement}
-            className="max-w-sm w-full lg:max-w-2/5 lg:flex"
+            className="max-w-sm w-full lg:max-w-2/5 lg:flex bg-gray-50"
           >
             <div className="border  rounded-lg p-6">
               <div className="mb-8">
-                <p className="text-gray-700 text-base italic text-slate-500">
+                <p className="text-gray-700 text-base  text-slate-500 leading-6">
                   {'" ' + item.clientStatement + ' " '}
                 </p>
               </div>
               <div className="flex items-center">
                 {/* <img className="w-10 h-10 rounded-full mr-4" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink"> */}
-                <div className="text-sm">
+                <div className="text-sm leading-6">
                   <p className="text-gray-700 leading-none">
                     {"~ " + item.clientName}
                   </p>
-                  <p className="text-gray-600">Aug{18}</p>
+                  <p className="text-gray-600">{item.testifyDate}</p>
                 </div>
               </div>
             </div>
